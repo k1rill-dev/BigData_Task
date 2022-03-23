c20cce6f059c497f68afaa364337afec8035296a
k1rill-dev <63358980+k1rill-dev@users.noreply.github.com>
2022-03-23 18:59:38 +0300

    add new parser

--- handlers/users/parse_price.py
+++ handlers/users/parse_price.py
@@ -1,51 +1,19 @@
 from aiogram import types
-from aiogram.dispatcher import FSMContext
-# from main import new_parse, parse_gend
-from states.state import StateBot
-from loader import dp
+from main import new_parse, parse_gend, price_checker
+from loader import dp, bot
 import time
-from datetime import datetime
-import requests
-from bs4 import BeautifulSoup
+import pandas as pd
 
-def parse_gend():
-  try:
-    current_datetime = datetime.now()
-    time = current_datetime.strftime("%d-%m-%Y %H:%M:%S")
-    url = 'http://gendalf.cf/'
-    r = requests.get(url=url)
-    r.encoding = 'utf-8'
-    response = r.text
-    soup = BeautifulSoup(response, 'lxml')
-    body = soup.find('body')
-    name = body.find('h1').text
-    price = body.find('h2').text.replace('Цена: ', '').replace('₽', '').replace(' ', '')
-    return time, int(price), name
-  except:
-    print('ливай дядя')
-
-def new_parse():
-  try:
-    current_datetime = datetime.now()
-    time = current_datetime.strftime("%d-%m-%Y %H:%M:%S")
-    url = 'http://gendalf.cf:8080/product/игровая-консоль-nintendo-switch-32-gb/'
-    r = requests.get(url=url)
-    r.encoding = 'utf-8'
-    response = r.text
-    soup = BeautifulSoup(response, 'lxml')
-    name = soup.find('div', class_='summary entry-summary').find('h1').text
-    price = soup.find('div', class_='summary entry-summary').find('span', class_='woocommerce-Price-amount amount').find('bdi').text
-    return time, name, int(price.split('.')[0])
-  except:
-    print('ливай дядя')
 
 @dp.message_handler(commands='check')
 async def check(message: types.Message):
   cur_price = parse_gend()[1]
   sec = 0
+  await message.answer(f"Время: {parse_gend()[0]} \n {parse_gend()[2]} \n Актуальная цена: {parse_gend()[1]}₽ \n Старая цена:{cur_price}₽ \n Разница: {cur_price - parse_gend()[1]}₽")
   while True:
     if cur_price != parse_gend()[1]:
-      msg = f"Время: {parse_gend()[0]} \n {parse_gend()[2]} \n Актуальная цена: {parse_gend()[1]}₽ \n Старая цена:{cur_price}₽ \n Разница: {cur_price - parse_gend()[1]}₽"
+      diff = cur_price - parse_gend()[1]
+      msg = f"Время: {parse_gend()[0]} \n {parse_gend()[2]} \n Актуальная цена: {parse_gend()[1]}₽ \n Старая цена:{cur_price}₽ \n Разница: {diff}₽"
       await message.answer(msg)
       cur_price = parse_gend()[1]
       sec = 0
@@ -63,6 +31,7 @@ async def check(message: types.Message):
 @dp.message_handler(commands="check_wp")
 async def check_wp(message: types.Message):
   cur_price_wp = new_parse()[2]
+  await message.answer(f"Время: {new_parse()[0]} \n {new_parse()[2]} \n Актуальная цена: {new_parse()[1]}₽ \n Старая цена:{cur_price_wp}₽ \n Разница: {cur_price_wp - new_parse()[1]}₽")
   sec = 0
   while True:
     if cur_price_wp != new_parse()[2]:
@@ -78,3 +47,15 @@ async def check_wp(message: types.Message):
         await message.answer('я задолбался ждать, хватит')
         break
       continue
+
+
+@dp.message_handler(commands="check_article")
+async def check_article(message: types.Message):
+    try:
+      await bot.send_photo(message.from_user.id, "https://s1.livelib.ru/boocover/1003156338/200/8be5/boocover.jpg")
+      data = price_checker()
+      await message.answer(f"Название книги: {data[0]}"+\
+                           f"\nЦена: {data[1]}")
+    except Exception as ex:
+      await message.answer(f"Ошибка - {ex}")
+
--- main.py
+++ main.py
@@ -8,9 +8,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 from fake_useragent import UserAgent
 from data.config import *
-
+import pandas as pd
 
 ua = UserAgent()
+
 
 options = webdriver.ChromeOptions()
 options.add_argument('text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9')
@@ -24,70 +25,94 @@ wd = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=options)
 
 
 def parse_gend():
-  try:
-    current_datetime = datetime.now()
-    time = current_datetime.strftime("%d-%m-%Y %H:%M:%S")
-    url = 'http://gendalf.cf/'
-    r = requests.get(url=url)
-    r.encoding = 'utf-8'
-    response = r.text
-    soup = BeautifulSoup(response, 'lxml')
-    body = soup.find('body')
-    name = body.find('h1').text
-    price = body.find('h2').text.replace('Цена: ', '').replace('₽', '').replace(' ', '')
-    return time, int(price), name
-  except:
-    print('ливай дядя')
+    try:
+        current_datetime = datetime.now()
+        time = current_datetime.strftime("%d-%m-%Y %H:%M:%S")
+        url = 'http://gendalf.cf/'
+        r = requests.get(url=url)
+        r.encoding = 'utf-8'
+        response = r.text
+        soup = BeautifulSoup(response, 'lxml')
+        body = soup.find('body')
+        name = body.find('h1').text
+        price = body.find('h2').text.replace('Цена: ', '').replace('₽', '').replace(' ', '')
+        return time, int(price), name
+    except:
+        print('ливай дядя')
+
 
 def new_parse():
-  try:
-    current_datetime = datetime.now()
-    time = current_datetime.strftime("%d-%m-%Y %H:%M:%S")
-    url = 'http://gendalf.cf:8080/product/игровая-консоль-nintendo-switch-32-gb/'
-    r = requests.get(url=url)
-    r.encoding = 'utf-8'
-    response = r.text
-    soup = BeautifulSoup(response, 'lxml')
-    name = soup.find('div', class_='summary entry-summary').find('h1').text
-    price = soup.find('div', class_='summary entry-summary').find('span', class_='woocommerce-Price-amount amount').find('bdi').text
-    return time, name, int(price.split('.')[0])
-  except:
-    print('ливай дядя')
+    try:
+        current_datetime = datetime.now()
+        time = current_datetime.strftime("%d-%m-%Y %H:%M:%S")
+        url = 'http://gendalf.cf:8080/product/игровая-консоль-nintendo-switch-32-gb/'
+        r = requests.get(url=url)
+        r.encoding = 'utf-8'
+        response = r.text
+        soup = BeautifulSoup(response, 'lxml')
+        name = soup.find('div', class_='summary entry-summary').find('h1').text
+        price = soup.find('div', class_='summary entry-summary').find('span',
+                                                                      class_='woocommerce-Price-amount amount').find(
+            'bdi').text
+        return time, name, int(price.split('.')[0])
+    except:
+        print('ливай дядя')
 
 
 def parse_imdb(film):
-  wd.get('https://www.imdb.com/')
-  find_textbox = wd.find_element(By.ID, 'suggestion-search')
-  btn = wd.find_element(By.ID, 'suggestion-search-button')
-  find_textbox.send_keys(film)
-  btn.click()
-  href = wd.find_element(By.XPATH, '//*[@id="main"]/div/div[2]/table/tbody/tr[1]/td[2]/a')
-  href.click()
-  name = wd.find_element(By.XPATH, '//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[1]/div[1]/h1').text
-  rait = wd.find_element(By.XPATH, '//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[1]/div[2]/div/div[1]/a/div/div/div[2]/div[1]').text
-  return name, rait
+    wd.get('https://www.imdb.com/')
+    find_textbox = wd.find_element(By.ID, 'suggestion-search')
+    btn = wd.find_element(By.ID, 'suggestion-search-button')
+    find_textbox.send_keys(film)
+    btn.click()
+    href = wd.find_element(By.XPATH, '//*[@id="main"]/div/div[2]/table/tbody/tr[1]/td[2]/a')
+    href.click()
+    name = wd.find_element(By.XPATH,
+                           '//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[1]/div[1]/h1').text
+    rait = wd.find_element(By.XPATH,
+                           '//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[1]/div[2]/div/div[1]/a/div/div/div[2]/div[1]').text
+    return name, rait
+
 
 def parse_kinopoisk(film):
     # никто не верил, что получится запарсить кинопоиск без selenium
     # меня недооценили)
     film = film.split()
     headers = {
-      'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36 OPR/83.0.4254.70'
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36 OPR/83.0.4254.70'
     }
     req = requests.get(f'https://www.kinopoisk.ru/index.php?kp_query={"+".join(film)}', headers=headers)
     response = req.text
     soup = BeautifulSoup(response, 'lxml')
     name = soup.find('div', class_='element most_wanted').find('div', class_='info').find('p', class_='name').text
-    reit = soup.find('div', class_='element most_wanted').find('div', class_='right').find('div',class_='rating ratingGreenBG').text
+    reit = soup.find('div', class_='element most_wanted').find('div', class_='right').find('div',
+                                                                                           class_='rating ratingGreenBG').text
     return name, reit
 
+
+def price_checker():
+    url = 'https://www.ozon.ru/product/devushka-s-tatuirovkoy-drakona-larsson-stig-250059673/?asb=Nagkc2QZl8PFX9CmVJ%252F0xSOdZQuGYKC%252FHEtTtJqD2xI%253D&asb2=rv1KP_Bp3rVq02k96yKDd82QXChnl7SqU7_w8T-w05l_pTiVWlQoeIv_eZLvmfsN&keywords=девушка+с+татуировкой+дракона&sh=sK0lCmxnfg'
+    headers = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36 OPR/83.0.4254.70'
+    }
+    req = requests.get(url, headers=headers)
+    response = req.text
+    # print(response)
+    soup = BeautifulSoup(response, 'lxml')
+    name = soup.find('div', class_='k0n k6n k4n').find('h1', class_='k2u').text
+    price = soup.find('div', class_='tk1').find('span', class_='kt0 k0t').text.replace('\u2009', '').replace('\xa0', '')
+    return name, price
+
+
 def main():
-    film = 'star wars'
-    # film = input()
-    print(parse_imdb(film))
-    # print(parse_kinopoisk(film))
+    # # film = 'star wars'
+    # # film = input()
+    # # print(parse_imdb(film))
+    # # print(parse_kinopoisk(film))
+    print(price_checker())
+
 
 if __name__ == "__main__":
     main()
-
